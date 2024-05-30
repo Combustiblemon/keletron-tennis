58b3a2f2ae44ddd44d69368549e814676909d1ec
Combustiblemon <comblmn@gmail.com>
2024-05-30 23:14:05 +0300

    pwa test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import withPWAInit from '@ducanh2912/next-pwa';
 
-export default nextConfig;
+const withWPA = withPWAInit({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  cacheStartUrl: true,
+  reloadOnOnline: true,
+});
+
+/** @type {import('next').NextConfig} */
+const NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withWPA(NextConfig);
