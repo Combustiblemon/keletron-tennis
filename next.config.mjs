b1873fe79674df8c3fbc21c93d065f4af2a78c02
Combustiblemon <comblmn@gmail.com>
2024-05-31 08:48:30 +0300

    manifest test

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import withPWAInit from 'next-pwa';
 
 const withWPA = withPWAInit({
   dest: 'public',
-  // disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
   cacheOnFrontEndNav: true,
   cacheStartUrl: true,
   reloadOnOnline: true,
