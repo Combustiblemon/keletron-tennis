fba9aed45013e281e3763776fa2570f5714df882
Combustiblemon <comblmn@gmail.com>
2024-08-04 23:22:33 +0300

    updated manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,24 @@ const Document = () => {
         <link rel="mask-icon" href="/logo.webp" color="#FFFFFF" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="apple-touch-icon" href="/logo.webp" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
         {/* <link
           rel="apple-touch-icon"
           sizes="152x152"
